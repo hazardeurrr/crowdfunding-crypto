05b93c9da10fd6cd749b7eee582af4955a502d73
Malcolm Etoundi <malcolm.etoundi@edu.devinci.fr>
2021-04-16 16:22:55 +0200

    ajoutPage campagne

--- startp-react/pages/services-1.js
+++ startp-react/pages/services-1.js
@@ -5,8 +5,14 @@ import PageBanner from '@/components/Common/PageBanner';
 import Link from 'next/link';
 import HTMLEditor from '@/components/Common/HTMLEditor';
 import * as Icon from 'react-feather';
+import "react-dates/initialize";
+import { DateRangePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
  
 const Services1 = () => {
+    const [startDate, setStartDate] = React.useState();
+    const [endDate, setEndDate] = React.useState();
+    const [focusedInput, setFocusedInput] = React.useState();
 
     return (
         <>
@@ -59,7 +65,7 @@ const Services1 = () => {
                                 <p><strong> Fudraising Duration </strong><br/> Projects with shorter durations have higher success rates. You won’t be able to adjust your duration after you launch.</p>
                                 <div className="col-lg-12 col-md-12">
                                     <div className="form-group">
-                                        <input
+                                        {/* <input
                                             id="date"
                                             label="Birthday"
                                             type="date"
@@ -68,6 +74,18 @@ const Services1 = () => {
                                             InputLabelProps={{
                                             shrink: true,
                                             }}
+                                        /> */}
+                                        <DateRangePicker
+                                            startDate={startDate}
+                                            startDateId="start-date"
+                                            endDate={endDate}
+                                            endDateId="end-date"
+                                            onDatesChange={({ startDate, endDate }) => {
+                                            setStartDate(startDate);
+                                            setEndDate(endDate);
+                                            }}
+                                            focusedInput={focusedInput}
+                                            onFocusChange={(focusedInput) => setFocusedInput(focusedInput)}
                                         />
                                     </div>
                                 </div>
@@ -87,9 +105,61 @@ const Services1 = () => {
                                     </div>
                                 </div>
                                 
+                                <p><strong> Raising Method </strong><br/>Give a raising currency for your crowdfunding project.</p>
+                                <div className="col-lg-12 col-md-12">
+                                    <div className="form-group">
+                                        <div className="order-details">
+                                            <div className="payment-method">
+                                                <p>
+                                                    <input type="radio" id="usdt" name="radio-group" defaultChecked/>
+                                                    <label htmlFor="usdt">USDT (Tether)</label>
+                                                </p>
+                                                <p>
+                                                    <input type="radio" id="eth" name="radio-group" />
+                                                    <label htmlFor="eth">ETH (Ether)</label>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <p><strong> Flexibilty </strong><br/>Indicate how flexible can you be on your fundraising and the amount you want to gather.</p>
+                                <div className="col-lg-12 col-md-12">
+                                    <div className="form-group">
+                                        <div className="order-detail">
+                                            <div className="payment-method">
+                                                <p>
+                                                    <input type="radio2" id="flexible" name="radio-group" defaultChecked/>
+                                                    <label htmlFor="flexible">Flexible</label>
+                                                </p>
+                                                <p>
+                                                    <input type="radio2" id="goal" name="radio-group" />
+                                                    <label htmlFor="goal">Goal has to be reached</label>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <h4 style={{marginTop: "15px", marginBottom: "20px"}}>Optionnal</h4>
+                                <p><strong> Tiers </strong><br/>Indicate differents levels of contribution for your fundraising.</p>
+                                <div className="col-lg-12 col-md-12">
+                                    <div className="form-group">
+                                        <input type="number" placeholder="Threshold" className="form-control" />
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" placeholder="Title" className="form-control" />
+                                    </div>
+                                    <div className="form-group">
+                                        <textarea name="message" cols="30" rows="6" placeholder="Description" className="form-control"></textarea>
+                                    </div>
+                                </div>
+
+                                
                                 <div className="col-lg-12 col-md-12">
                                     <button className="btn btn-primary" type="submit">Create Fundraiser !</button>
                                 </div>
+
                             </div>
                         </form>
                     </div>
