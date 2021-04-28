a804b29bfba2a64457ffbfefe6d28b47bce7d0e9
Malcolm Etoundi <malcolm.etoundi@edu.devinci.fr>
2021-04-28 14:47:42 +0200

    setProfile add

--- startp-react/pages/setProfile.js
+++ startp-react/pages/setProfile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from "@/components/_App/Navbar";
 import Footer from "@/components/_App/Footer";
 import PageBanner from '@/components/Common/PageBanner';
@@ -12,6 +12,24 @@ import profiles from '@/utils/usersListJson.json';
 
 const SetProfile = (props) => {
     // const data = undefined;
+    function getJson() {
+        const user = profiles.users.find(e => e.eth_address == "0x899657553381574")
+        console.log("user : " + user.eth_address)
+    }
+
+    // console.log(JSON.stringify(profiles))
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [bio, setBio] = useState('');
+    const [twitter, setTwitter] = useState('');
+    const [site, setSite] = useState('');
+
+    const handleChangeName = (event) => setName(event.target.value);
+    const handleChangeEmail = (event) => setEmail(event.target.value);
+    const handleChangeBio = (event) => setBio(event.target.value);
+    const handleChangeTwitter = (event) => setTwitter(event.target.value);
+    const handleChangeSite = (event) => setSite(event.target.value);
 
     return (
         <>
@@ -29,19 +47,21 @@ const SetProfile = (props) => {
 
                     <div className="faq-contact">
                         <h3>Complete the information about your profile</h3>
-                        <form>
+                        <form onSubmit={() => {
+                            // alert(name + email + bio + twitter + site)
+                        }}>
                             <div className="row">
                                 <p><strong> Displayed name </strong><br/>Choose a name that will be displayed to the other users</p>
                                 <div className="col-lg-12 col-md-12">
                                     <div className="form-group">
-                                        <input type="text" placeholder="Name" className="form-control" />
+                                        <input type="text" placeholder="Name" className="form-control" value={name} onChange={handleChangeName}/>
                                     </div>
                                 </div>
 
                                 <p><strong> Email </strong><br/>Enter your email address</p>
                                 <div className="col-lg-12 col-md-12">
                                     <div className="form-group">
-                                        <input type="email" placeholder="Email" className="form-control" />
+                                        <input type="email" placeholder="Email" className="form-control" value={email} onChange={handleChangeEmail}/>
                                     </div>
                                 </div>
 
@@ -55,28 +75,37 @@ const SetProfile = (props) => {
                                 <p><strong> Bio </strong><br/> Make a short description of yourself.</p>
                                 <div className="col-lg-12 col-md-12">
                                     <div className="form-group">
-                                        <textarea name="bio" cols="30" rows="6" placeholder="Bio" className="form-control"></textarea>
+                                        <textarea name="bio" cols="30" rows="6" placeholder="Bio" className="form-control" value={bio} onChange={handleChangeBio}></textarea>
                                     </div>
                                 </div>
 
                                 <p><strong> Twitter account </strong><br/>Link your Twitter account to be certified</p>
                                 <div className="col-lg-12 col-md-12">
                                     <div className="form-group">
-                                        <input type="text" placeholder="@" className="form-control" />
+                                        <input type="text" placeholder="@" className="form-control" value={twitter} onChange={handleChangeTwitter}/>
                                     </div>
                                 </div>
 
                                 <p><strong> Website </strong></p>
                                 <div className="col-lg-12 col-md-12">
                                     <div className="form-group">
-                                        <input type="url" name="url" id="url" pattern="https://.*" placeholder="https://your-site.com" className="form-control" />
+                                        <input type="url" name="url" id="url" pattern="https://.*" placeholder="https://your-site.com" className="form-control" value={site} onChange={handleChangeSite}/>
                                     </div>
                                 </div>
 
                                 <div className="col-lg-12 col-md-12">
                                     <button className="btn btn-primary" type="submit" onClick={() => {
-                                        const user = profiles.users.find(e => e.eth_address == "0x899657553381574")
-                                        console.log(user.eth_address)
+                                        const user = profiles.users.find(e => e.eth_address == "0x899657553381574");
+                                        user.username = name;
+                                        user.email = email;
+                                        user.bio = bio;
+                                        user.twitter = twitter;
+                                        user.website = site;
+
+                                        const newData = JSON.stringify(profiles);
+
+                                        alert(newData)
+
                                     }}>Update Profile</button>
                                 </div>
                             </div>
