16925228ca8cc367f51ac3ad71318bfd027d9af1
hazardeurrr <thibault.drouot@gmail.com>
2021-05-24 11:27:44 +0200

    remove

--- startp-react/pages/it-startup.js
+++ startp-react/pages/it-startup.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import Navbar from "@/components/_App/Navbar";
 import MainBanner from "@/components/MachineLearning/MainBanner";
-import Features from "@/components/ITStartup/Features";
-import OurFeatures from "@/components/ITStartup/OurFeatures";
-import ServicesArea from "@/components/ITStartup/ServicesArea";
-import Team from "@/components/Common/Team";
-import FunFactsArea from "@/components/Common/FunFactsArea";
-import RecentWorks from "@/components/Common/RecentWorks";
-import PricingStyleOne from "@/components/PricingPlans/PricingStyleOne";
-import Feedback from "@/components/Common/Feedback";
-import Partner from "@/components/Common/Partner";
-import BlogPost from "@/components/Common/BlogPost";
 import Footer from "@/components/_App/Footer";
-import CategoriesMainPage from "@/components/ITStartup/CategoriesMainPage";
 import CategoriesLight from "@/components/ITStartup/CategoriesLight";
 import ProjectGridMain from "@/components/ITStartup/ProjectGridMain";
 import DiscoverArea from '@/components/ITStartup/DiscoverArea';
@@ -35,9 +24,7 @@ const ITStartup = () => {
             <FunFactsArea />
             <RecentWorks />
             <PricingStyleOne />
-            <Feedback />
             <Partner /> */}
-            {/* <BlogPost /> */}
             {/* <CategoriesMainPage /> */}
 
             <Footer />
