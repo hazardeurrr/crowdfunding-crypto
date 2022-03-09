46c3139cb85d3991f84f47aac8f693d1a09e42da
hazardeurrr <thibault.drouot@gmail.com>
2022-03-09 16:32:51 +0100

    changes on contact us

--- startp-react/components/Contact/ContactInfo.js
+++ startp-react/components/Contact/ContactInfo.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import * as Icon from 'react-feather';
+import Link from 'next/link';
 
 const ContactInfo = () => {
     return (
         <div className="contact-info-area ptb-80">
             <div className="container">
+                <div className="section-title">
+                    <h2>Get In Touch With Us</h2>
+                        <div className="bar"></div>
+                        <p>If you have any question, please make sure it's not already answered on our <Link href="/how-it-works"><a>"How it works"</a></Link> page.</p>
+
+                </div>
                 <div className="row justify-content-center">
                     <div className="col-lg-4 col-md-6 col-sm-6">
                         <div className="contact-info-box">
+
                             <div className="icon">
                                 <Icon.Mail />
                             </div>
-                            <h3>Mail Here</h3>
-                            <p><a href="mailto:admin@startp.com">admin@startp.com</a></p>
-                            <p><a href="mailto:info@startp.com">info@startp.com</a></p>
+                            <h3>Support</h3>
+                            <p>We’ll Be Glad To Assist You !</p>
+                            <p><a href="mailto:support@blockboosted.com?subject=[SUPPORT]">support@blockboosted.com</a></p>
                         </div>
                     </div>
 
                     <div className="col-lg-4 col-md-6 col-sm-6">
+                        <div className="contact-info-box">
+                            <div className="icon">
+                                <Icon.ThumbsUp />
+                            </div>
+                            <h3>Feedback</h3>
+                            <p>Give us your thoughts about BlockBoosted</p>
+                            <p><a href="mailto:feedback@blockboosted.com?subject=[FEEDBACK]">feedback@blockboosted.com</a></p>
+                        </div>
+                    </div>
+
+                    {/* <div className="col-lg-4 col-md-6 col-sm-6">
                         <div className="contact-info-box">
                             <div className="icon">
                                 <Icon.MapPin />
@@ -36,7 +55,7 @@ const ContactInfo = () => {
                             <p><a href="tel:+1234567890">+123 456 7890</a></p>
                             <p><a href="tel:+2414524526">+241 452 4526</a></p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
