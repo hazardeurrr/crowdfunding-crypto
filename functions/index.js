a7e71f94b7452b2741c1e9f071c9388e9f8d408f
Malcolm Etoundi <malcolm@MacBook-Pro-de-Malcolm.local>
2022-03-22 11:28:32 +0100

    amount okk

--- functions/index.js
+++ functions/index.js
@@ -302,68 +302,68 @@ const cors = require('cors')({ origin: true });
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 
-exports.updateWeeklyTotals = functions.region('europe-west1').pubsub.schedule('every monday 00:01')
-  .timeZone('Europe/Paris')
-  .onRun(async(context) => {
-    var now = parseInt(Math.floor(Date.now() / 1000))
-    const web3Instance = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/391e7c4cd5274ef8a269414b4833bade"))
-    await web3Instance.eth.getBlockNumber().then(async(currentBlock) => {
-			console.log("currentblock", currentBlock)
-			await db.collection('utils').doc('lastWeeklyUpdate').get().then(async(doc1) => {
-				let lastWeeklyUpdate = doc1.data()
-				let lastTimeStamp = lastWeeklyUpdate.timestamp
-				let lastBlockUpdated = lastWeeklyUpdate.block
-				//console.log(lastWeeklyUpdate)
-				let compteur = 0
-				const rewardCtr = new web3Instance.eth.Contract(rewardAbi, rewardAddr);
-				rewardCtr.methods.getStartTimestamp().call().then(async(startTs) =>{
-					// console.log("start timestamp retrieve : " + startTs)
-					// console.log("now" + now)
-					let midTs = (now + lastTimeStamp) / 2
-					 console.log(lastTimeStamp + " ts")
-					// console.log(lastBlockUpdated + " block")
-					// console.log(midTs + " mid")
-					let week = parseInt(Math.floor((midTs - startTs) / 604800))
-					console.log(" week computed =>" + week)
-					await db.collection('utils').doc('rates').get().then(async(doc2) => {
-						let rate = doc2.data()
-						rewardCtr.getPastEvents("Participate", ({fromBlock: lastBlockUpdated, toBlock: currentBlock - 1}))
-						.then((events) => {
-							let eventsFiltered = events.filter(e => e.returnValues.timestamp > lastTimeStamp)
-							compteur += eventsFiltered.reduce((a,b) => {
-								let currentRate = 1
-								if(e.returnValues.token == "0x0000000000000000000000000000000000000000")
-									currentRate = rate.eth[week] == undefined ? 1 : rate.eth[week]
-								if(e.returnValues.token == "0x67c0fd5c30C39d80A7Af17409eD8074734eDAE55")
-									currentRate = rate.bbst[week] == undefined ? 1 : rate.bbst[week]
-								if(e.returnValues.token == "0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b")
-									currentRate = rate.usdc[week] == undefined ? 1 : rate.usdc[week] * 10**12
-								return a.returnValues.amount * currentRate + b.returnValues.amount * currentRate
-							}, 0)
-						});
+// exports.updateWeeklyTotals = functions.region('europe-west1').pubsub.schedule('every monday 00:01')
+//   .timeZone('Europe/Paris')
+//   .onRun(async(context) => {
+//     var now = parseInt(Math.floor(Date.now() / 1000))
+//     const web3Instance = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/391e7c4cd5274ef8a269414b4833bade"))
+//     await web3Instance.eth.getBlockNumber().then(async(currentBlock) => {
+// 			console.log("currentblock", currentBlock)
+// 			await db.collection('utils').doc('lastWeeklyUpdate').get().then(async(doc1) => {
+// 				let lastWeeklyUpdate = doc1.data()
+// 				let lastTimeStamp = lastWeeklyUpdate.timestamp
+// 				let lastBlockUpdated = lastWeeklyUpdate.block
+// 				//console.log(lastWeeklyUpdate)
+// 				let compteur = 0
+// 				const rewardCtr = new web3Instance.eth.Contract(rewardAbi, rewardAddr);
+// 				rewardCtr.methods.getStartTimestamp().call().then(async(startTs) =>{
+// 					// console.log("start timestamp retrieve : " + startTs)
+// 					// console.log("now" + now)
+// 					let midTs = (now + lastTimeStamp) / 2
+// 					 console.log(lastTimeStamp + " ts")
+// 					// console.log(lastBlockUpdated + " block")
+// 					// console.log(midTs + " mid")
+// 					let week = parseInt(Math.floor((midTs - startTs) / 604800))
+// 					console.log(" week computed =>" + week)
+// 					await db.collection('utils').doc('rates').get().then(async(doc2) => {
+// 						let rate = doc2.data()
+// 						rewardCtr.getPastEvents("Participate", ({fromBlock: lastBlockUpdated, toBlock: currentBlock - 1}))
+// 						.then((events) => {
+// 							let eventsFiltered = events.filter(e => e.returnValues.timestamp > lastTimeStamp)
+// 							compteur += eventsFiltered.reduce((a,b) => {
+// 								let currentRate = 1
+// 								if(e.returnValues.token == "0x0000000000000000000000000000000000000000")
+// 									currentRate = rate.eth[week] == undefined ? 1 : rate.eth[week]
+// 								if(e.returnValues.token == "0x67c0fd5c30C39d80A7Af17409eD8074734eDAE55")
+// 									currentRate = rate.bbst[week] == undefined ? 1 : rate.bbst[week]
+// 								if(e.returnValues.token == "0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b")
+// 									currentRate = rate.usdc[week] == undefined ? 1 : rate.usdc[week] * 10**12
+// 								return a.returnValues.amount * currentRate + b.returnValues.amount * currentRate
+// 							}, 0)
+// 						});
 					
 			
-						console.log(compteur)
+// 						console.log(compteur)
 			
-						await db.collection('utils').doc('rewardData').get().then(doc3 => {
-							let rewardData = doc3.data()
-						//	console.log(rewardData)
-							let newTotalPerWeek = rewardData.totalPerWeek
-							newTotalPerWeek.push(compteur)
-							db.collection('utils').doc('rewardData').update({totalPerWeek: newTotalPerWeek})
-							db.collection('utils').doc('lastWeeklyUpdate').update({timestamp: now, block: currentBlock})
-						})
-					})
-				})
-			})
-		})
-  return null;
-});
+// 						await db.collection('utils').doc('rewardData').get().then(doc3 => {
+// 							let rewardData = doc3.data()
+// 						//	console.log(rewardData)
+// 							let newTotalPerWeek = rewardData.totalPerWeek
+// 							newTotalPerWeek.push(compteur)
+// 							db.collection('utils').doc('rewardData').update({totalPerWeek: newTotalPerWeek})
+// 							db.collection('utils').doc('lastWeeklyUpdate').update({timestamp: now, block: currentBlock})
+// 						})
+// 					})
+// 				})
+// 			})
+// 		})
+//   return null;
+// });
 
 exports.getClaimValueSigned = functions.region('europe-west1').https.onRequest((request, response) => {
   cors(request, response, () => {
   var eventsTmp = []
-	const web3Instance = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/391e7c4cd5274ef8a269414b4833bade"))
+  const web3Instance = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/391e7c4cd5274ef8a269414b4833bade"))
   var claim = 0;
   const rewardCtr = new web3Instance.eth.Contract(rewardAbi, rewardAddr);
 //	console.log(rewardCtr)
@@ -375,15 +375,19 @@ exports.getClaimValueSigned = functions.region('europe-west1').https.onRequest((
   rewardCtr.methods.getLastClaim(userAddr).call().then((res) => {
     rewardCtr.getPastEvents("Participate", ({fromBlock: parseInt(res)}))
       .then((events) => {
-        console.log("Events : " + events);
 
-        rewardCtr.methods.getStartTimestamp().call().then((time) => {
+		let eventsFiltered = events.filter(e => e.returnValues.user == userAddr);
+        rewardCtr.methods.getStartTimestamp().call().then(async(time) => {
 
-          claim += events.map(async(e) => {
+          const promises = eventsFiltered.map(async(e) => {
 
-              var week = parseInt(Math.floor((e.returnValues.timestamp - time) / 604800));
-              var rate = await db.collection('utils').doc('rates').get();
-              rate.then(async(resRate) => {
+			console.log("Current event :", e.returnValues.timestamp);
+
+			var week = parseInt(Math.floor((e.returnValues.timestamp - time) / 604800)) == 0 ? 0 : 0;
+			var total = 0;
+			await db.collection('utils').doc('rates').get().then(async(resRate) => {
+
+				console.log("Rate crypto :", resRate);
 
                 var currentRate = 0
 
@@ -394,33 +398,55 @@ exports.getClaimValueSigned = functions.region('europe-west1').https.onRequest((
                 if(e.returnValues.token == "0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b")
                   currentRate = resRate.data().usdc[week]
 
-                var rewardData = await db.collection('utils').doc('rewardData').get();
-                var total = 0;
-                rewardData.then((data) => {
+                await db.collection('utils').doc('rewardData').get().then((data) => {
 
-                  console.log(data.data());
+				  console.log("week :", week)
 
                   if (data.data().totalPerWeek[week] == 0) {
                     eventsTmp.push(e.returnValues.campaign);
                   } else {
                     var ratio = ((e.returnValues.amount * currentRate) / data.data().totalPerWeek[week]) > 0.03 ? 0.03 : ((e.returnValues.amount * currentRate) / data.data().totalPerWeek[week]);
-                    total += ratio * data.data()[week];
+					console.log("ratio :", ratio);
+                    total += ratio * data.data().weeklySupply[week];
                     console.log("Total : " + total);
                   }
 
-                  console.log(parseInt(total));
+				}).catch((error) => {console.log(error)})
 
-                  return parseInt(total);
+			}).catch((error) => {console.log(error)})
 
-                })
-              })
-          }).reduce(((a,b) => a + b), 0);
+			return total;
+		  })
 
-          console.log("Claim :", claim);
+			await Promise.all(promises).then((map) => {
+				claim += map.reduce(((a,b) => a + b), 0);
+			})
 
-          response.json({result: `${claim}`});
+			console.log("Claim :", claim);
+
+			const recipient = {
+				address: userAddr,
+				allocation: claim
+			}
+		
+			console.log(recipient);
+
+			const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/391e7c4cd5274ef8a269414b4833bade"))
+
+			const message = web3.utils.soliditySha3(
+				{t: 'address', v: recipient.address},
+				{t: 'uint256', v: recipient.allocation.toString()}
+			).toString('hex');
+		
+			const { signature } = web3.eth.accounts.sign(
+				message, 
+				process.env.PRIVATE_KEY
+			);
+		
+			response.json({message: `${message}`, result: `${signature}`});
         })
-      });
+	});
+
 
 
 
@@ -433,17 +459,17 @@ exports.getClaimValueSigned = functions.region('europe-west1').https.onRequest((
 });
 });
 
-exports.tryWeb3 = functions.region('europe-west1').https.onRequest(async(request, response) => {
-	const web3Instance = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/391e7c4cd5274ef8a269414b4833bade"))
-	const rewardCtr = new web3Instance.eth.Contract(rewardAbi, rewardAddr);
-				rewardCtr.methods.getStartTimestamp().call().then(async(startTs) => {
-					console.log(startTs)
-					response.json({result: `startTs${startTs}.`});
+// exports.tryWeb3 = functions.region('europe-west1').https.onRequest(async(request, response) => {
+// 	const web3Instance = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/391e7c4cd5274ef8a269414b4833bade"))
+// 	const rewardCtr = new web3Instance.eth.Contract(rewardAbi, rewardAddr);
+// 				rewardCtr.methods.getStartTimestamp().call().then(async(startTs) => {
+// 					console.log(startTs)
+// 					response.json({result: `startTs${startTs}.`});
 
-				}).catch(err => {console.log(err); response.json({result: `Fail.`})})
+// 				}).catch(err => {console.log(err); response.json({result: `Fail.`})})
 
 
-});
+// });
 
 
 
