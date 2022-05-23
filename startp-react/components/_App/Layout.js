b75d81a5d338273f1d0601406fa0726d2853357d
hazardeurrr <thibault.drouot@gmail.com>
2022-05-23 12:42:38 +0200

    changes meta

--- startp-react/components/_App/Layout.js
+++ startp-react/components/_App/Layout.js
@@ -12,7 +12,7 @@ const Layout = ({ children }) => {
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content="BlockBoosted is an innovative web3 crowdfunding platform. Raise & donate in crypto with 0% fee, get rewarded in BBST." />
+                <meta name="description" content="BlockBoosted · Crypto crowdfunding platform · Raise & donate in crypto with 0% fee · Get rewarded in BBST · Discover & support ambitious projects" />
                 <title>BlockBoosted - Boost your project with crypto</title>
             </Head>
             {children}
