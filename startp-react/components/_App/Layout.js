05045bf12e6a4aac94ec05d5fdc45d29a8cfff2e
hazardeurrr <thibault.drouot@gmail.com>
2022-05-23 14:31:19 +0200

    s

--- startp-react/components/_App/Layout.js
+++ startp-react/components/_App/Layout.js
@@ -56,7 +56,7 @@ const Layout = ({ children }, {c, crea}) => {
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <title>BlockBoosted - Boost your project with crypto</title>
+                <title>BlockBoosted App - Boost your project with crypto</title>
             </Head>
             {children}
 
