96edde12106ac1581c883f69c12e15512b2cfe84
hazardeurrr <thibault.drouot@gmail.com>
2022-10-04 15:25:45 +0200

    how it works lil upd

--- startp-react/pages/how-it-works.js
+++ startp-react/pages/how-it-works.js
@@ -208,7 +208,7 @@ const ServiceDetails = () => {
                                                     The easiest way to get assets on BNB Smart Chain (BSC) is to make a withdrawal of your selected currency from Binance or any other exchanges that supports BSC. <b>Make sure to select BNB Smart Chain (BSC) as selected network when you make your withdrawal !</b><br></br><br></br>
                                                     If you have assets on other chains and want to bridge them to the BSC network, please use <a href="https://www.bnbchain.org/en/bridge">the BNB Smart Chain Bridge</a><br></br><br></br>
                                                     If you already have some crypto on BSC network, you can directly use it on our platform (BNB or BUSD)
-                                                    or swap it on a DEX/CEX that supports BSC.
+                                                    or swap it on a DEX/CEX that supports BSC to BNB or BUSD.
                                                     <br></br><br></br><Icon.AlertTriangle />&nbsp;<i>Note that you will need BNB to pay the gas fees once using the BNB Smart Chain network.</i>
                                                 </p>
                                             </AccordionItemPanel>
